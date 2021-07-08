5b5ca2c0bfd1799f59d98f1a1a94c8043611eb6b
Rick <dirklo@gmail.com>
2021-07-08 13:54:12 +0200

    test explicit classnames

--- components/ChildComponent2.js
+++ components/ChildComponent2.js
@@ -5,7 +5,7 @@ export default function ChildComponent() {
     return (
         <div>
             <main className={styles.main}>
-                <h1>I AM THE SECOND PAGE</h1>
+                <h1 className={styles.main_h1}>I AM THE SECOND PAGE</h1>
                 <Image
                     src='/image2.jpg'
                     alt='Image 2'
